033f7e01e77b1c32f578b870bc019c44eb60e9ad
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-06-06 14:28:31 +0530

    firebase integrated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:profile_management_app/config/app_router/app_routes.dart';
 import 'package:profile_management_app/config/app_theme/app_theme.dart';
+import 'package:profile_management_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
